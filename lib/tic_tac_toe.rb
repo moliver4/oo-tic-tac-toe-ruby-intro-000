8a93527d88959ccc61e99dab0bba32dbd5e1bae0
Tiffany Tang <tifftang22@gmail.com>
2019-07-14 03:12:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ class TicTacToe
     @index = (input.to_i - 1)
   end
 
-  def move(@index, current_player = "X")
+  def move(index, current_player = "X")
     @board[@index] = current_player
   end
 
